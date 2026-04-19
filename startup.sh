75d6f722b6360b0bedb0dbc091307dccf08b9a02
EliabDuran4 <supereliab54@gmail.com>
2026-04-19 13:47:14 -0600

    rapido express

--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Comando de inicio para Azure App Service
-# Azure ejecuta este archivo automáticamente al iniciar la app
-
-gunicorn --bind=0.0.0.0:8000 --timeout 120 --workers 2 app:app
